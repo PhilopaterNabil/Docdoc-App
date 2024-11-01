9c5a79c5c9f791521d7af306da34dcfb61283e45
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-11-02 00:04:42 +0200

    Refactor main entry points for development and production environments
    Ensure proper screen size initialization for both development and production environments by adding `ScreenUtil.ensureScreenSize()` before running the app. This change ensures that the app adapts correctly to different screen sizes and resolutions.
    
    Additionally, the original `main.dart` file has been removed as it is no longer needed, with its functionality now split between `main_development.dart` and `main_production.dart`.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced/core/di/dependency_injection.dart';
 import 'package:flutter_advanced/doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/routing/app_router.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(appRouter: AppRouter()));
 }
