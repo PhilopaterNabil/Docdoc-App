7898bf3a3c28f365eeb6dcd543546145433a3713
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2025-03-29 00:24:13 +0200

    Refactor login repository to use implementation class and update dependency injection

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_advanced/core/networking/api_service.dart';
 import 'package:flutter_advanced/core/networking/dio_factory.dart';
 import 'package:flutter_advanced/features/login/data/repos/login_repo.dart';
+import 'package:flutter_advanced/features/login/data/repos/login_repo_impl.dart';
 import 'package:flutter_advanced/features/login/logic/cubit/login_cubit.dart';
 import 'package:flutter_advanced/features/sign_up/data/repos/sign_up_repo.dart';
 import 'package:flutter_advanced/features/sign_up/logic/cubit/sign_up_cubit.dart';
@@ -13,7 +14,7 @@ Future<void> setupGetIt() async {
   Dio dio = DioFactory.getDio();
   getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
 
-  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(apiService: getIt()));
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepoImpl(apiService: getIt()));
   getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
 
   getIt
---
+++ lib/features/login/data/repos/login_repo_impl.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_advanced/core/networking/api_error_handler.dart';
+import 'package:flutter_advanced/core/networking/api_result.dart';
+import 'package:flutter_advanced/core/networking/api_service.dart';
+import 'package:flutter_advanced/features/login/data/models/login_request_body.dart';
+import 'package:flutter_advanced/features/login/data/models/login_response.dart';
+import 'package:flutter_advanced/features/login/data/repos/login_repo.dart';
+
+class LoginRepoImpl implements LoginRepo {
+  final ApiService apiService;
+
+  LoginRepoImpl({required this.apiService});
+
+  @override
+  Future<ApiResult<LoginResponse>> login(LoginRequestBody loginRequestBody) async {
+    try {
+      final response = await apiService.login(loginRequestBody);
+      return ApiResult.success(response);
+    } catch (e) {
+      return ApiResult.failure(ErrorHandler.handle(e));
+    }
+  }
+}
