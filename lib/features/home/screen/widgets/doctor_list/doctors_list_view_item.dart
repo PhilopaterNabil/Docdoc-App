035797e76e0484637b5499f4ba28ad5a33e4e3c4
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-08 17:24:40 +0300

    feat: Replace ClipRRect with CachedNetworkImage for improved image loading and add shimmer effect during loading

--- lib/features/home/screen/widgets/doctor_list/doctors_list_view_item.dart
+++ lib/features/home/screen/widgets/doctor_list/doctors_list_view_item.dart
@@ -1,8 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced/core/helpers/spacing.dart';
+import 'package:flutter_advanced/core/theming/colors.dart';
 import 'package:flutter_advanced/core/theming/styles.dart';
 import 'package:flutter_advanced/features/home/data/models/specializations_response_model.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class DoctorsListViewItem extends StatelessWidget {
   const DoctorsListViewItem({super.key, this.doctorModel});
@@ -15,32 +18,45 @@ class DoctorsListViewItem extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 16.h),
       child: Row(
         children: [
-          // ClipRRect(
-          //   borderRadius: BorderRadius.circular(16.r),
-          //   child: CachedNetworkImage(
-          //     imageUrl: 'https://images.unsplash.com/photo-1537368910025-700350fe46c7',
-          //     progressIndicatorBuilder: (context, url, progress) => Center(
-          //       child: CircularProgressIndicator(
-          //         value: progress.progress,
-          //         strokeWidth: 2.w,
-          //       ),
-          //     ),
-          //     errorWidget: (context, url, error) => Icon(
-          //       Icons.error,
-          //       size: 24.w,
-          //       color: Colors.red,
-          //     ),
-          //   ),
-          // ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16.r),
-            child: Image.network(
-              'https://images.unsplash.com/photo-1537368910025-700350fe46c7',
+          CachedNetworkImage(
+            imageUrl: 'https://images.unsplash.com/photo-1537368910025-700350fe46c7',
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Shimmer.fromColors(
+                baseColor: ColorsManager.lightGray,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            imageBuilder: (context, imageProvider) => Container(
               width: 110.w,
               height: 120.h,
-              fit: BoxFit.cover,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
+          // ClipRRect(
+          //   borderRadius: BorderRadius.circular(16.r),
+          //   child: Image.network(
+          //     'https://images.unsplash.com/photo-1537368910025-700350fe46c7',
+          //     width: 110.w,
+          //     height: 120.h,
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
 
           horizontalSpace(16),
           Expanded(
