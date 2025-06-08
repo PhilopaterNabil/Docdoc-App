3720871d00116ad6070aa7f529ba1ba3b2609efe
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-08 13:23:50 +0300

    feat: Integrate flutter_secure_storage for secure token management and update related dependencies

--- lib/core/networking/dio_factory.dart
+++ lib/core/networking/dio_factory.dart
@@ -31,7 +31,7 @@ class DioFactory {
   static Future<void> addDioHeaders() async {
     dio?.options.headers = {
       'Accept': 'application/json',
-      'Authorization': 'Bearer ${await SharedPrefHelper.getString(SharedPrefKeys.userToken)}',
+      'Authorization': 'Bearer ${await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken)}',
     };
   }
 
